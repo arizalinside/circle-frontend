241b3c97f829a90bc49d955a9702af4d402c639d
Arizal Sykes <arizalinc@gmail.com>
2020-09-27 19:28:41 +0700

    ✨ Create room components

---
+++ circle-app/src/components/Room.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="room">
+    <b-container>
+      <b-row>
+        <b-col sm="12" class="empty-room">
+          <p>Please select a chat to start messaging</p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Room'
+}
+</script>
+
+<style scoped src="../assets/css/home.css">
+</style>
