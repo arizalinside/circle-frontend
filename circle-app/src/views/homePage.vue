0b7e39e7808385fd06028a699fe4717cf77aaeb3
Arizal Sykes <arizalinc@gmail.com>
2020-09-27 19:27:50 +0700

    🚚 Moved room to components

--- circle-app/src/views/homePage.vue
+++ circle-app/src/views/homePage.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="home">
-    <b-container class="bv-example-row">
+    <b-container fluid class="bv-example-row">
       <b-row>
-        <b-col sm="4">Telegram</b-col>
-        <b-col sm="8">Please select chat to start messaging</b-col>
+        <List />
+        <!-- <b-col sm="4">
+          <b-col sm="10" class="logo-text">Circle</b-col>
+          <b-col sm="2" class="hamburger">
+            <img src="@/assets/image/menu.png" />
+          </b-col>
+        </b-col> -->
+        <Room />
       </b-row>
     </b-container>
   </div>
 </template>
 
 <script>
+import List from '../components/List'
+import Room from '../components/Room'
+
 export default {
-  name: 'homePage'
+  name: 'homePage',
+  components: {
+    List,
+    Room
+  }
 }
 </script>
