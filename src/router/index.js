20f542b2e7dc549c9d81adedcf230843a9ba78ab
Arizal Sykes <arizalinc@gmail.com>
2020-10-23 22:32:38 +0700

    set interceptor req and res

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import registerPage from '../views/registerPage.vue'
 import loginPage from '../views/loginPage.vue'
 import homePage from '../views/homePage.vue'
+import store from '../store'
 
 Vue.use(VueRouter)
 
@@ -10,17 +11,20 @@ const routes = [
   {
     path: '/register',
     name: 'registerPage',
-    component: registerPage
+    component: registerPage,
+    meta: { requiresVisitor: true }
   },
   {
     path: '/login',
     name: 'loginPage',
-    component: loginPage
+    component: loginPage,
+    meta: { requiresVisitor: true }
   },
   {
-    path: '/home',
+    path: '/',
     name: 'homePage',
-    component: homePage
+    component: homePage,
+    meta: { requiresAuth: true }
   }
 ]
 
@@ -28,6 +32,31 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    if (!store.getters.isLogin) {
+      next({
+        path: '/login'
+        // query: { redirect: to.fullPath }
+      })
+    } else {
+      next()
+    }
+  } else if (to.matched.some(record => record.meta.requiresVisitor)) {
+    if (store.getters.isLogin) {
+      next({
+        path: '/'
+      })
+    } else {
+      next()
+    }
+  } else {
+    next() // make sure to always call next()!
+  }
 })
 
 export default router
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,16 +1,25 @@
 import axios from 'axios'
+import router from '../../router/index'
 
 export default {
   state: {
     user: {},
     token: localStorage.getItem('token') || null,
-    user_id: ''
+    user_id: '',
+    errorLogin: ''
   },
   mutations: {
     setUser(state, payload) {
       state.user = payload
       state.token = payload.token
       state.user_id = payload.user_id
+    },
+    setError(state, payload) {
+      state.errorLogin = payload
+    },
+    delUser(state) {
+      state.user = {}
+      state.token = null
     }
   },
   actions: {
@@ -42,9 +51,63 @@ export default {
             reject(error.response)
           })
       })
+    },
+    logout(context) {
+      localStorage.removeItem('token')
+      context.commit('delUser')
+      router.push('/login')
+    },
+    interceptorRequest(context) {
+      axios.interceptors.request.use(
+        function(config) {
+          config.headers.Authorization = `Bearer ${context.state.token}`
+          // Do something before request is sent
+          return config
+        },
+        function(error) {
+          // Do something with request error
+          return Promise.reject(error)
+        }
+      )
+    },
+    interceptorResponse(context) {
+      axios.interceptors.response.use(
+        function(response) {
+          // Any status code that lie within the range of 2xx cause this function to trigger
+          // Do something with response data
+          return response
+        },
+        function(error) {
+          if (error.response.status === 403) {
+            if (
+              error.response.data.msg === 'invalid token' ||
+              error.response.data.msg === 'invalid siganture'
+            ) {
+              localStorage.removeItem('token')
+              context.commit('delUser')
+              router.push('/login')
+              alert(
+                'Sorry, your token is wrong and you cannot continue to this page'
+              )
+            } else if (error.response.data.msg === 'jwt expired') {
+              localStorage.removeItem('token')
+              context.commit('delUser')
+              router.push('/login')
+              alert('Sorry, your session is timeout')
+            }
+          }
+          return Promise.reject(error)
+        }
+      )
     }
   },
   getters: {
+    isLogin(state) {
+      return state.token !== null
+    },
+    getError(state) {
+      return state.errorLogin
+    },
     getUser(state) {
       return state.user
     },
