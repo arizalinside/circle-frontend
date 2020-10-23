d68cf7ae103b3f5b7d474be1e22d3f019122d7ce
Arizal Sykes <arizalinc@gmail.com>
2020-10-23 20:31:33 +0700

    fixing styling

--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -2,14 +2,18 @@
   <div class="home">
     <b-container fluid class="bv-example-row">
       <b-row>
-        <List />
+        <b-col sm="4" style="padding:0;">
+          <List />
+        </b-col>
         <!-- <b-col sm="4">
           <b-col sm="10" class="logo-text">Circle</b-col>
           <b-col sm="2" class="hamburger">
             <img src="@/assets/image/menu.png" />
           </b-col>
         </b-col> -->
-        <Room />
+        <b-col sm="8" style="padding:0;">
+          <Room />
+        </b-col>
       </b-row>
     </b-container>
   </div>
