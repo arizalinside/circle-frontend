dda6af54d33edef94be4dacff194fddacbb4597f
Arizal Sykes <arizalinc@gmail.com>
2020-11-03 15:34:20 +0700

    add persisted state

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Auth from './modules/auth'
+import User from './modules/user'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    Auth
+    Auth,
+    User
   },
   state: {},
   mutations: {},
   actions: {},
-  getters: {}
+  getters: {},
+  plugins: [
+    createPersistedState({
+      paths: ['Auth.user'],
+      storage: window.sessionStorage
+    })
+  ]
 })
