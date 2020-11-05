e2cfce106128ddc2e2ae6bc27ef5442f946058c6
Arizal Sykes <arizalinc@gmail.com>
2020-11-05 23:52:43 +0700

    import vue google maps and geo location

--- src/main.js
+++ src/main.js
@@ -8,10 +8,19 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap-css-only/css/bootstrap.min.css'
 // import 'mdbvue/lib/css/mdb.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
+import * as VueGoogleMaps from 'vue2-google-maps'
+import VueGeolocation from 'vue-browser-geolocation'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.config.productionTip = false
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyAftBoRJkmd8YT1Z5t97jzB7CMG7dGEPNw',
+    libraries: 'places'
+  }
+})
+Vue.use(VueGeolocation)
 
 new Vue({
   router,
