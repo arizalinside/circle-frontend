1be2763b4f65185ebdadcfca02d6cc8764a7d91f
Arizal Sykes <arizalinc@gmail.com>
2020-11-09 14:20:30 +0700

    add vuetimeago

--- src/main.js
+++ src/main.js
@@ -9,12 +9,14 @@ import 'bootstrap-css-only/css/bootstrap.min.css'
 // import 'mdbvue/lib/css/mdb.min.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPaperPlane } from '@fortawesome/free-regular-svg-icons'
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import * as VueGoogleMaps from 'vue2-google-maps'
 import VueGeolocation from 'vue-browser-geolocation'
+import VueTimeago from 'vue-timeago'
 
-library.add(faPaperPlane)
+library.add(faPaperPlane, faChevronLeft)
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
@@ -27,6 +29,16 @@ Vue.use(VueGoogleMaps, {
   }
 })
 Vue.use(VueGeolocation)
+Vue.use(VueTimeago, {
+  name: 'Timeago', // Component name, `Timeago` by default
+  locale: 'en', // Default locale
+  // We use `date-fns` under the hood
+  // So you can use all locales from it
+  locales: {
+    'zh-CN': require('date-fns/locale/zh_cn'),
+    ja: require('date-fns/locale/ja')
+  }
+})
 
 new Vue({
   router,
