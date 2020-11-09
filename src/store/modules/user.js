09f97c79b8031ee7da077d070d55702b0dec34b0
Arizal Sykes <arizalinc@gmail.com>
2020-11-09 14:24:17 +0700

    update friendstatus

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,8 +1,14 @@
 import axios from 'axios'
 
 export default {
-  state: {},
-  mutations: {},
+  state: {
+    friendStatus: 0
+  },
+  mutations: {
+    setFriendStatus(state, payload) {
+      state.friendStatus = payload
+    }
+  },
   actions: {
     updateUser(context, payload) {
       return new Promise((resolve, reject) => {
@@ -20,5 +26,36 @@ export default {
       })
     }
   },
-  getters: {}
+  updateStatus(context, payload) {
+    return new Promise((resolve, reject) => {
+      axios
+        .patch(`http://127.0.0.1:3000/user/update-status/${payload}`)
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.log(error.response)
+        })
+    })
+  },
+  getFriendStatus(context, payload) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`http://127.0.0.1:3000/user/status/${payload}`)
+        .then(response => {
+          console.log(response)
+          context.commit('setFriendStatus', response.data.data.user_status)
+          resolve(response.data)
+        })
+        .catch(error => {
+          console.log(error.response)
+          reject(error.response)
+        })
+    })
+  },
+  getters: {
+    getFriendStatus(state) {
+      return state.friendStatus
+    }
+  }
 }
